ab02f79ebfb29a4f7e65897df115dab4c135f98b
najmuleimon <najmul.eimon@gmail.com>
2022-02-28 22:49:18 +0600

    display details

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ const displayPhones = phones => {
                     <div class="card-body">
                         <h5 class="card-title">${phone.phone_name}</h5>
                         <h3>${phone.brand}</h3>
-                        <button onclick="showDetails(${phone.slug})" data-bs-toggle="modal" data-bs-target="#staticBackdrop">show details</button>
+                        <button onclick="showDetails('${phone.slug}')" data-bs-toggle="modal" data-bs-target="#staticBackdrop">show details</button>
                     </div>
                 </div>
             </div>
@@ -40,5 +40,36 @@ const showDetails = id => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayDetails(data.data))
+}
+
+const displayDetails = detail => {
+    const phoneDetails = document.getElementById('phone-details')
+    phoneDetails.innerHTML = `
+        <img src="${detail.image}" alt="">
+        <h3>${detail.name}</h3>
+        <h5>${detail.releaseDate}</h5>
+        <h5>${detail.brand}</h5>
+        <p>${detail.mainFeatures.displaySize}</p>
+        <p>${detail.mainFeatures.chipSet}</p>
+        <p>${detail.mainFeatures.memory}</p>
+        <p>Sensors</p>
+        <ul>
+            <li>${detail.mainFeatures.sensors[0]}</li>
+            <li>${detail.mainFeatures.sensors[1]}</li>
+            <li>${detail.mainFeatures.sensors[2]}</li>
+            <li>${detail.mainFeatures.sensors[3]}</li>
+            <li>${detail.mainFeatures.sensors[4]}</li>
+            <li>${detail.mainFeatures.sensors[5]}</li>
+        </ul>
+        <p>Others</p>
+        <ul>
+            <li>${detail.others.WLAN}</li>
+            <li>${detail.others.Bluetooth}</li>
+            <li>${detail.others.GPS}</li>
+            <li>${detail.others.NFC}</li>
+            <li>${detail.others.Radio}</li>
+            <li>${detail.others.USB}</li>
+        </ul>
+    `
 }
