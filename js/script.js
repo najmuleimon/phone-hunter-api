7790531f702be677a0617756e142064806af0ba3
najmuleimon <najmul.eimon@gmail.com>
2022-02-28 23:46:12 +0600

    error handel

--- js/script.js
+++ js/script.js
@@ -1,41 +1,64 @@
+// error message
+const errorMsg = document.getElementById('error-msg')
+
+// search phones
 const searchPhone = () => {
     const searchField = document.getElementById('search-phone')
     const searchText = searchField.value
     searchField.value = ''
 
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    fetch(url)
-    .then(response => response.json())
-    .then(data => displayPhones(data.data))
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+
+    if(searchText === ''){
+        errorMsg.innerText = 'Please write phone names to search'
+    }
+    else{
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        fetch(url)
+        .then(response => response.json())
+        .then(data => displayPhones(data.data))
+    }
 }
 
+// display search phones
 const displayPhones = phones => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
 
-    phones.forEach(phone => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="card">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="${phone.image}" class="img-fluid rounded-start" alt="">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">${phone.phone_name}</h5>
-                        <h3>${phone.brand}</h3>
-                        <button onclick="showDetails('${phone.slug}')" data-bs-toggle="modal" data-bs-target="#staticBackdrop">show details</button>
+    if(phones.length === 0){
+        errorMsg.innerText = 'No phone found'
+    }
+    else{
+        let firstTwenty = phones.slice(0, 20)
+        errorMsg.innerText = `show ${firstTwenty.length} results of total ${phones.length} products of your search`
+
+        firstTwenty.forEach(phone => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card">
+                <div class="row g-0">
+                    <div class="col-md-6">
+                        <img src="${phone.image}" class="img-fluid rounded-start" alt="">
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card-body">
+                            <h5 class="card-title">${phone.phone_name}</h5>
+                            <h3>${phone.brand}</h3>
+                            <button onclick="showDetails('${phone.slug}')" data-bs-toggle="modal" data-bs-target="#staticBackdrop">show details</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        `;
-        searchResult.appendChild(div);
-    })
+            `;
+            searchResult.appendChild(div);
+        })
+    }
+    
 }
 
+// show details of phones
 const showDetails = id => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
     fetch(url)
@@ -43,12 +66,14 @@ const showDetails = id => {
     .then(data => displayDetails(data.data))
 }
 
+
+// display phone details in modals
 const displayDetails = detail => {
     const phoneDetails = document.getElementById('phone-details')
     phoneDetails.innerHTML = `
         <img src="${detail.image}" alt="">
         <h3>${detail.name}</h3>
-        <h5>${detail.releaseDate}</h5>
+        <h5>${detail.releaseDate ? detail.releaseDate : 'Release date not found'}</h5>
         <h5>${detail.brand}</h5>
         <p>${detail.mainFeatures.displaySize}</p>
         <p>${detail.mainFeatures.chipSet}</p>
