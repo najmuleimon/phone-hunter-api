a61bd52e1204bc03b69350cb377d440462f38a27
najmuleimon <najmul.eimon@gmail.com>
2022-03-01 21:24:57 +0600

    responsive done

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 // error message
-const errorMsg = document.getElementById('error-msg')
+const errorMsg = document.getElementById('error-msg');
 
-// loader
+// loader toggle
 const toggleSpinner = displayStyle => {
     document.getElementById('spinner').style.display = displayStyle;
 }
 
+
+
 // search phones
 const searchPhone = () => {
-    const searchField = document.getElementById('search-phone')
-    const searchText = searchField.value.toLowerCase()
-    searchField.value = ''
+    const searchField = document.getElementById('search-phone');
+    const searchText = searchField.value.toLowerCase();
+    searchField.value = '';
 
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
 
-    toggleSpinner('block');
-
     if(searchText === ''){
-        errorMsg.innerText = 'Please write phone names to search'
+        errorMsg.innerText = 'Please write phone names to search';
     }
     else{
+        toggleSpinner('block');
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
         fetch(url)
         .then(response => response.json())
@@ -36,12 +37,12 @@ const displayPhones = phones => {
     toggleSpinner('block');
     
     if(phones.length === 0){
+        errorMsg.innerText = 'No phone found';
         toggleSpinner('none');
-        errorMsg.innerText = 'No phone found'
     }
     else{
-        let firstTwenty = phones.slice(0, 20)
-        errorMsg.innerText = `Showing ${firstTwenty.length} results of total ${phones.length} products of your search`
+        let firstTwenty = phones.slice(0, 20);
+        errorMsg.innerText = `Showing ${firstTwenty.length} results of total ${phones.length} products of your search`;
 
         firstTwenty.forEach(phone => {
             const div = document.createElement('div');
@@ -74,35 +75,37 @@ const showDetails = id => {
 
 // display phone details in modals
 const displayDetails = detail => {
-    const phoneDetails = document.getElementById('phone-details')
-    phoneDetails.textContent = ''
-    const div = document.createElement('div')
-    div.innerHTML = `
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = '';
+    phoneDetails.innerHTML = `
+    <div class="col-lg-5 text-center">
         <img src="${detail.image}" alt="">
+    </div>
+    <div class="col-lg-7">
         <h3>${detail.name}</h3>
         <h5>${detail.releaseDate ? detail.releaseDate : 'Release date not found'}</h5>
-        <h5>${detail.brand}</h5>
-        <p>${detail.mainFeatures.displaySize}</p>
-        <p>${detail.mainFeatures.chipSet}</p>
-        <p>${detail.mainFeatures.memory}</p>
-        <p>Sensors</p>
-        <ul>
-            <li>${detail.mainFeatures.sensors[0]}</li>
-            <li>${detail.mainFeatures.sensors[1]}</li>
-            <li>${detail.mainFeatures.sensors[2]}</li>
-            <li>${detail.mainFeatures.sensors[3]}</li>
-            <li>${detail.mainFeatures.sensors[4]}</li>
+        <h5><span>Brand:</span>${detail.brand}</h5>
+        <p><span>Display:</span>${detail.mainFeatures.displaySize}</p>
+        <p><span>Chipset:</span>${detail.mainFeatures.chipSet}</p>
+        <p><span>Memory:</span>${detail.mainFeatures.memory}</p>
+        <h4>Sensors:</h4>
+        <ul class="sensors">
+            <li>${detail.mainFeatures.sensors[0]},</li>
+            <li>${detail.mainFeatures.sensors[1]},</li>
+            <li>${detail.mainFeatures.sensors[2]},</li>
+            <li>${detail.mainFeatures.sensors[3]},</li>
+            <li>${detail.mainFeatures.sensors[4]},</li>
             <li>${detail.mainFeatures.sensors[5]}</li>
         </ul>
-        <p>Others</p>
-        <ul>
-            <li>${detail.others?.WLAN ? detail.others.WLAN : ''}</li>
-            <li>${detail.others?.Bluetooth ? detail.others.Bluetooth : ''}</li>
-            <li>${detail.others?.GPS ? detail.others.GPS : ''}</li>
-            <li>${detail.others?.NFC ? detail.others.NFC : ''}</li>
-            <li>${detail.others?.Radio ? detail.others.Radio : ''}</li>
-            <li>${detail.others?.USB ? detail.others.USB : ''}</li>
+        <h4>Others:</h4>
+        <ul class="others">
+            <li><span>WLAN:</span>${detail.others?.WLAN ? detail.others.WLAN : 'Not Found'}</li>
+            <li><span>Bluetooth:</span>${detail.others?.Bluetooth ? detail.others.Bluetooth : 'Not Found'}</li>
+            <li><span>GPS:</span>${detail.others?.GPS ? detail.others.GPS : 'Not Found'}</li>
+            <li><span>NFC:</span>${detail.others?.NFC ? detail.others.NFC : 'Not Found'}</li>
+            <li><span>Radio:</span>${detail.others?.Radio ? detail.others.Radio : 'Not Found'}</li>
+            <li><span>USB:</span>${detail.others?.USB ? detail.others.USB : 'Not Found'}</li>
         </ul>
+    </div>
     `
-    phoneDetails.appendChild(div)
 }
